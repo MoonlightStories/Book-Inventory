2456848683d57fe39309433e9d84c37b88b7b9c5
liltheekshana <148948875+liltheekshana@users.noreply.github.com>
2024-09-13 09:43:41 +0530

    Delete cw (2) - Copy/cw/cw directory

--- cw (2) - Copy/cw/cw/src/main/java/com/example/cw/data/BookRepositary.java
+++
@@ -1,9 +0,0 @@
-package com.example.cw.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepositary {
-
-    interface BookRepository extends JpaRepository<Book, Long> {
-    }
-}
